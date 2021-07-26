caac5ec2bb8aa823a585587704f87dfbf173e14c
Guillermo Gonzalez Duque <guillermo.gonzalez.duque@gmail.com>
2021-07-26 16:58:37 +0200

    StudentController added

---
+++ src/main/java/de/neuefische/spring/demo/Controller.java
@@ -0,0 +1,33 @@
+package de.neuefische.spring.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+
+public class Controller {
+/*
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @PutMapping("/hello")
+    public String helloPut(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @DeleteMapping("/hello")
+    public String helloDelete(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+ */
+
+
+}

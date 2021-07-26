0857e7b90688843fcf1a94846fa9977bad4a3361
Guillermo Gonzalez Duque <guillermo.gonzalez.duque@gmail.com>
2021-07-26 15:26:24 +0200

    Initial Spring commit

---
+++ src/test/java/de/neuefische/spring/demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
